eb8ad28499cf15d8b1af6f06d1f15d5fa5934a92
Gregory Norris <gnor522@gmail.com>
2021-08-03 21:10:05 -0400

    basic framework setup

---
+++ R/load_exercise.R
@@ -0,0 +1,24 @@
+#'Helper Function to Load Files Containing
+#'
+#'@description This function looks at the file extension to guess at what it should be loading
+#'
+
+load_exercise =
+  function(){
+  ###Leverage a switch for multiple file formats
+  }
+
+#'Create a exercise list class system to leverage S3
+#'
+#'
+#'
+
+construct_exercise =
+  function(){
+  ###Create a new tibble class exercise_table
+  }
+
+#' Validate the constructor class
+#'
+#'
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(WorkoutCreator)
+
+test_check("WorkoutCreator")
